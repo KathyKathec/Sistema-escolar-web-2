e1e3f75bd95e48150c8f75cd162a4cab70fb3fe5
Katherin <katherinpereda19@hotmail.com>
2025-09-02 20:57:41 -0300

    Primeiro commit

---
+++ src/main/java/X/Etapa1/Etapa1Application.java
@@ -0,0 +1,13 @@
+package X.Etapa1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Etapa1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Etapa1Application.class, args);
+	}
+
+}
---
+++ src/test/java/X/Etapa1/Etapa1ApplicationTests.java
@@ -0,0 +1,13 @@
+package X.Etapa1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Etapa1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
